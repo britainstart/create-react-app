f307280582cdaac33d675724a4cf249703d484e5
britainstart <bcs5505@psu.edu>
2023-09-26 13:10:12 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -27,60 +27,6 @@ function App() {
   <button id="colorBtn">BGColorChanger</button>
   <button id="Headingbtn">HeadingChanger</button>
   <button id ="dltbtn">DeleteCard</button>
-  <script>
-   document.querySelector('#btn').addEventListener('click', function(e){
-    clone = document.querySelector('#card').cloneNode('card')}
-document.body.appendChild(clone);
-     const cardCline = cardToClone.cloneNode(true);
-{'}'})
-</script> 
-   <script>
-        var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-        var letters = ["A", "B", "C", "D", "E", "F"];
-        function randomColor() {
-            color = ""}
-                index= Math.floor(Math.random() * (numbers.length + letters.length));
-                color+=(index)?numbers[index]:letters[index-numbers.length];
-            {'}'}
-            return color;
-        {'}'}
-       document.querySelector('#colorBtn').addEventListener('click', function (e) {
-            card = document.querySelector('.card')}
-   
-            const currentColor = card.style.backgroundColor;
-             card.style.backgroundColor = '#' + randomColor();
-        {'}'});
-    </script>
-  <script>
-   document.querySelector('#Headingbtn').addEventListener('click', function(e){
-      title = document.querySelector('h4')}
-      title.innerText = "Super Pod 2 Realty";
-    {'}'})
-  </script>
-  <script>
-    //card delete
-    document.querySelector('#dltbtn').addEventListener('click', function(e){
-      cards = document.querySelectorAll('.card')}
-      
-    if (cards.length {'>'} 2) {
-        lastCard = cards[cards.length - 1]}
-        lastCard.parentNode.removeChild(lastCard);
-    {'}'}
-    {'}'})
-  </script>
-  <script>
-    
-    var original= document.querySelector('#description').innerText;
-document.querySelector('#detailsBtn').addEventListener('click', function(e) {
-    description = document.querySelector('#description')}
-    
-    if (description.innerText === 'none' || description.innerText=== '') {
-        description.innerText = original}
-    {'}'} else {
-        description.innerText = ''}
-    {'}'}
-{'}'});
-  </script>
 </html> 
   );
   }
